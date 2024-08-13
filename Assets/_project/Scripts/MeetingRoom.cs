53229ff83271e79e9624fd0716273f48485a1127
Arnaud Candaele <arnaud.candaele@buildwise.be>
2024-08-13 10:20:57 +0200

    Fix the average meetings per day computation

--- Assets/_project/Scripts/MeetingRoom.cs
+++ Assets/_project/Scripts/MeetingRoom.cs
@@ -20,8 +20,8 @@ public class MeetingRoom : MonoBehaviour
     public List<string> Organizers { get; set; } = null;
     public List<string> Titles { get; set; } = null;
 
-    public DateTime DateStartFilter { get; set; } = DateTime.MinValue;
-    public DateTime DateEndFilter { get; set; } = DateTime.MaxValue;
+    public DateTime DateStartFilter { get; set; } = new DateTime(2024,01,01);
+    public DateTime DateEndFilter { get; set; } = new DateTime(2024, 08, 15);
 
     /// <summary>
     /// Gets the average number of meetings held in the room per day, between DateStartFilter and DateEndFilter included.
@@ -32,6 +32,7 @@ public class MeetingRoom : MonoBehaviour
         Debug.Log($"Calculating average meetings per day for room {RoomNumber} between {DateStartFilter} and {DateEndFilter}.");
         if (StartTimes == null || StartTimes.Count == 0)
         {
+            Debug.LogWarning($"Room {RoomNumber} has no start times.");
             return 0;
         }
         var filteredStartTimes = StartTimes
@@ -42,10 +43,32 @@ public class MeetingRoom : MonoBehaviour
             )
             .ToList();
 
+        /*
         var occurrencesPerDay = filteredStartTimes
             .GroupBy(startTime => startTime.Value.Date)
             .Select(group => new { Date = group.Key, Occurrences = group.Count() })
             .Average(group => group.Occurrences);
+        */
+        /*
+        var occurrencesPerDay = filteredStartTimes
+            .GroupBy(startTime => startTime.Value.Date)
+            .Select(group => new { Date = group.Key, Occurrences = group.Count() });
+        */
+        int numberOfMeetings = StartTimes.Count;
+        int numberOfDays = (DateEndFilter - DateStartFilter).Days + 1;
+        float occurrencesPerDay = (float)numberOfMeetings / (float)numberOfDays;
+
+        /*
+        float meetings = 0.0f;
+        for (DateTime date = DateStartFilter.Date; date <= DateEndFilter.Date; date.AddDays(1.0))
+        {
+            var result = occurrencesPerDay.FirstOrDefault(group => date == group.Date);
+            if (result != null)
+            {
+                meetings += result.Occurrences;
+            }
+        }
+        */
 
         Debug.Log($"Room {RoomNumber} has {occurrencesPerDay} occurrences per day between {DateStartFilter} and {DateEndFilter}.");
 
